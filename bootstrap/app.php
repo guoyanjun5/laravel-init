906a9cf11bd034e20c18ced7f2b2bfe75f86565c
guoyanjun <guoyanjun@ooyby.com>
2020-08-31 16:14:30 +0800

    difference environment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -43,13 +43,24 @@ $app->singleton(
 
 /*
 |--------------------------------------------------------------------------
-| Return The Application
+| Return The Application php.ini 增加配置项  
 |--------------------------------------------------------------------------
 |
 | This script returns the application instance. The instance is given to
 | the calling script so we can separate the building of the instances
 | from the actual running of the application and sending responses.
 |
-*/
+| app_env=dev 			//开发环境
+| app_env=testing 		//测试环境
+| app_env=staging 		//预发布环境
+| app_env=production 	//生产环境
+|
+*/	
+
+$env = get_cfg_var('app_env');
+
+$env = !!$env ? $env : 'production';
+
+$app->loadEnvironmentFrom('.env.'.$env);
 
 return $app;
